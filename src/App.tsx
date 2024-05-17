d99bec787ef03c61c27046c4df095b8aaf81ec40
Youssef Hafnawy <77534098+ymhaah@users.noreply.github.com>
2024-05-17 23:40:33 +0300

    Add Header and Hero Components
    - Header component with navigation menu
    - Hero component
    - Fixed issues and enhanced the Button component:
      - Ensured consistent variant styling.
      - Improved accessibility by properly handling aria attributes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+<<<<<<< Updated upstream
+=======
+import Header from "./components/Header.tsx";
+import Hero from "./components/Hero.tsx";
+>>>>>>> Stashed changes
 
 import './App.css'
 function App() {
+<<<<<<< Updated upstream
   return (
     <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
   )
+=======
+    return (
+        <>
+            <Header />
+            <Hero/>
+        </>
+    );
+>>>>>>> Stashed changes
 }
 
 export default App
